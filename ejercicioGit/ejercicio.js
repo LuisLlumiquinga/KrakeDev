402637f8fb3af966e22af8e028c6d754b40b68cf
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-09-16 16:14:15 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,4 +1,8 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/perder.gif";
+    
     console.log("ingresar a saludar");
 }
 
