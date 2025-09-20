600b21061d6421ba47648347574c32aab179c434
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-09-20 12:55:58 -0500

    actualizacion promedio notas Reto 32

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -10,9 +10,17 @@ calcularPromedioNotas=function(){
 
     cambiarTexto("lblPromedio", promedio.toFixed(2));
 
-    if(promedio>7){
-        cambiarImagen("imgResultado", "./exito.gif")
+    if(promedio<5 && promedio>0){
+        cambiarTexto("lblMensaje","REPROBADO");
+        cambiarImagen("imgResultado", "./reprobado.gif")
+    }else if(promedio>=5 && promedio<=8){
+        cambiarTexto("lblMensaje","BUEN TRABAJO");
+        cambiarImagen("imgResultado", "./buen trabajo.gif");
+    }else if(promedio>8 && promedio<=10){
+        cambiarTexto("lblMensaje","EXCELENTE");
+        cambiarImagen("imgResultado", "./excelente.gif");
     }else{
-        cambiarImagen("imgResultado", "./fracaso.gif");
+        cambiarTexto("lblMensaje","DATOS INCORRECTOS");
+        cambiarImagen("imgResultado", "./error.gif");
     }
 }
