419788e736f5b12e547221a266b2971db3bb7ead
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-09-23 15:35:48 -0500

    JUEGO_AHORCADO_V1 PASO 1 completado

--- Modulo 1/JUEGO_AHORCADO_V1/ahorcado.js
+++ Modulo 1/JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,10 +1,35 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta = "";
 
-esMayuscula=function(caracter){
-    if(caracter>=65 && caracter<=90){
-        return true;
-    }else{
-        return false;
+esMayuscula = function (caracter) {
+  if (caracter >= 65 && caracter <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+guardarPalabra = function () {
+  let palabra;
+  let cont = 0;
+  let mayuscula = false;
+
+  palabra = recuperarTexto("txtSecreta");
+
+  if (palabra.length == 5) {
+    for (let i = 0; i < 5; i++) {
+      if (esMayuscula(palabra.charCodeAt(i))) {
+        cont++;
+      }
     }
-}
 
+    if (cont == palabra.length) {
+      palabraSecreta = palabra;
+      console.log(palabraSecreta);
+    } else {
+      alert("DEBE INGRESAR UNA PALABRA DE 5 LETRAS MAYUSCULAS");
+    }
+  } else {
+    alert("DEBE INGRESAR UNA PALABRA DE 5 LETRAS MAYUSCULAS");
+  }
+};
