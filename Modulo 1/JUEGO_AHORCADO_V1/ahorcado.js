f350db7b2a67ca503b79d20842be29d8cdea56b7
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-09-23 15:11:21 -0500

    creacion carpeta JUEGO_AHORCADO_V1 y PASO 0 completado

---
+++ Modulo 1/JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,10 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+
+esMayuscula=function(caracter){
+    if(caracter>=65 && caracter<=90){
+        return true;
+    }else{
+        return false;
+    }
+}
+
