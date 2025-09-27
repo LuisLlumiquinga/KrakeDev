ac13f509f400cf929d466c45f98a3a197a85965f
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-09-27 08:21:21 -0500

    Reto 46 - Evidencia Roles Parte 2, COMPLETO

--- 02. Modulo 1/roles/roles.js
+++ 02. Modulo 1/roles/roles.js
@@ -1,12 +1,35 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0101145457",nombre:"Joselyne",apellido:"Morales",sueldo:800.0}
 ]
+
+mostrarEmpleados=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    let contenidoTabla="<table><tr>"+
+    "<th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th>"+
+    "</tr>";
+    for(let i=0; i<empleados.length; i++){
+        elementoEmpleado=empleados[i];
+        contenidoTabla+="<tr><td>"+elementoEmpleado.cedula+"</td>"
+        +"<td>"+elementoEmpleado.nombre+"</td>"
+        +"<td>"+elementoEmpleado.apellido+"</td>"
+        +"<td>"+elementoEmpleado.sueldo+"</td>"
+        +"</tr>"
+    }
+    contenidoTabla+="</table>"
+    cmpTabla.innerHTML=contenidoTabla;
+}
 
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol=function(){
