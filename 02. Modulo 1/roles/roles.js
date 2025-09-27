90734640d94e24fae09496a5c4ffc0a2de7cf770
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-09-27 12:23:15 -0500

    Reto 47-Roles Parte 3, COMPLETADO hasta literal 3

--- 02. Modulo 1/roles/roles.js
+++ 02. Modulo 1/roles/roles.js
@@ -39,15 +39,6 @@ mostrarOpcionEmpleado=function(){
     deshabilitarComponente("txtSueldo");
 }
 
-ejecutarNuevo=function(){
-    habilitarComponente("txtCedula");
-    habilitarComponente("txtNombre");
-    habilitarComponente("txtApellido");
-    habilitarComponente("txtSueldo");
-    habilitarComponente("btnGuardar");
-    esNuevo=true;
-}
-
 mostrarOpcionRol=function(){
     mostrarComponente("divRol");
     ocultarComponente("divEmpleado");
@@ -58,4 +49,115 @@ mostrarOpcionResumen=function(){
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
+}
+
+ejecutarNuevo=function(){
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+    esNuevo=true;
+}
+
+buscarEmpleado=function(cedula){
+    let elementoEmpleado;
+    let empleadoEncontrado=null;
+
+    for(let i=0; i<empleados.length; i++){
+        elementoEmpleado=empleados[i];
+        if(elementoEmpleado.cedula==cedula){
+            empleadoEncontrado=elementoEmpleado;
+            break;
+        }
+    }
+
+    return empleadoEncontrado;
+}
+
+agregarEmpleado=function(empleado){
+    let empleadoEncontrado=buscarEmpleado(empleado.cedula);
+    let empleadoAgregado=false;
+
+    if(empleadoEncontrado==null){
+        empleados.push(empleado);
+        empleadoAgregado=true;
+    }
+
+    return empleadoAgregado;
+}
+
+guardar=function(){
+    let cedula;
+    let nombre;
+    let apellido;
+    let sueldo;
+
+    cedula=recuperarInt("txtCedula");
+    let cedulaCorrecta=false;
+
+    if(isNaN(cedula)){
+        mostrarTexto("lblErrorCedula", "Cedula incorrecta, debe ingresar solo digitos");
+    }else if(cedula.toString().length!=10){
+        mostrarTexto("lblErrorCedula", "Cedula incorrecta, debe tener 10 digitos");
+    }else if(cedula==""){
+        mostrarTexto("lblErrorCedula", "Cedula incorrecta, ingrese 10 digitos");
+    }else{
+        cedulaCorrecta=true;
+    }
+
+    nombre=recuperarTexto("txtNombre");
+    let nombreMayuscula=false;
+
+    if(nombre.length>=3){
+        for(let i=0; i<nombre.length; i++){
+            if(nombre.charCodeAt(i)>=65 && nombre.charCodeAt(i)<=90){
+                nombreMayuscula=true;
+            }else{
+                mostrarTexto("lblErrorNombre", "El nombre debe tener minimo 3 caracteres y todos en mayuscula");
+            }                
+        }
+    }
+
+    apellido=recuperarTexto("txtApellido");
+    let apellidoMayuscula=false;
+
+    if(apellido.length>=3){
+        for(let i=0; i<apellido.length; i++){
+            if(apellido.charCodeAt(i)>=65 && apellido.charCodeAt(i)<=90){
+                apellidoMayuscula=true;
+            }else{
+                mostrarTexto("lblErrorApellido", "El apellido debe tener minimo 3 caracteres y todos en mayuscula");
+            }
+        }
+    }
+
+    sueldo=recuperarFloat("txtSueldo");
+    let sueldoCorrecto=false;
+
+    if(sueldo>=400 && sueldo<=5000){
+        sueldoCorrecto=true;
+    }else{
+        mostrarTexto("lblErrorSueldo", "El sueldo esta entre entre 400 y 5000");
+    }
+
+    if(cedulaCorrecta==true && nombreMayuscula==true && apellidoMayuscula==true && sueldoCorrecto==true){
+        if(esNuevo==true){
+            let empleado={};
+
+            empleado.cedula=cedula;
+            empleado.nombre=nombre;
+            empleado.apellido=apellido;
+            empleado.sueldo=sueldo;
+
+            let nuevoEmpleado=agregarEmpleado(empleado);
+
+            if(nuevoEmpleado==true){
+                alert("EMPLEADO GUARDADO CORRECTAMENTE");
+                mostrarEmpleados();
+            }else{
+                alert("YA EXISTE UN EMPLEADO CON LA CEDULA "+empleado.cedula);
+            }
+        }
+    }
 }
