5d0d48f6af67ad6c4205712885d5961506047acd
Luis Miguel Llumiquinga Llumiquinga <121265247+LuisLlumiquinga@users.noreply.github.com>
2025-11-17 20:26:46 -0500

    Reto 36 - Exponer REST WS, OK

---
+++ Servidor Web II/WorkspaceModulo3/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -0,0 +1,20 @@
+package com.krakedev.servicios;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("clientes")
+public class ServicioClientes {
+	
+	@Path("metodo1")
+	@GET
+	public String saludar() {
+		return "Hola mundo Rest Web Services";
+	}
+	
+	@Path("m1")
+	@GET
+	public String saludarDesdeOtraRuta() {
+		return saludar();
+	}
+}
