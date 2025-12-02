a6ee8e315340cc5b3e49b9c736a86e20919d9b21
Luis Miguel Llumiquinga Llumiquinga <121265247+LuisLlumiquinga@users.noreply.github.com>
2025-12-01 20:59:45 -0500

    Clase 01-12-2025

--- 02. Modulo 1/Clase 01-12-2025/repaso.js
+++ 02. Modulo 1/Clase 01-12-2025/repaso.js
@@ -1,3 +1,29 @@
-calificar = function (){
-    console.log("hola mundo")
+modificar = function (){
+    console.log("hola mundo");
+    alert("Se va a acabar el mundo, arrepientete pecador!!");
+
+    let texto=document.getElementById("texto");
+    texto.innerText="Mi profesor se llama Santiago";
+
+    let boton=document.getElementById("boton");
+    boton.value="CAMBIO";
+
+    let imagen=document.getElementById("imagen");
+    imagen.src="./Goofy_transparent.webp";
+}
+
+cambiar = function (){
+    let imagen1=document.getElementById("imagen1");
+    imagen1.src="./triste.webp";
+
+    let imagen2=document.getElementById("imagen2");
+    imagen2.src="./feliz.png";
+}
+
+reiniciar = function (){
+    let imagen1=document.getElementById("imagen1");
+    imagen1.src="./feliz.png";
+
+    let imagen2=document.getElementById("imagen2");
+    imagen2.src="./triste.webp";
 }
